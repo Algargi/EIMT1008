c7309a41c0d6804b48df80ee8abec1387da25c5b
Alvaro <algargi251103@gmail.com>
2024-02-06 20:21:17 +0100

    Ejercicio6 sin acabar

--- src/Practica1/Ejercicio6.java
+++ src/Practica1/Ejercicio6.java
@@ -1,7 +1,21 @@
 package Practica1;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
-
+        //NO SE, NO QUIERO PENSAR MÁS
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número entero: ");
+        int num = sc.nextInt();
+        for (int i = num-1; i >=1 ; i--) {
+            boolean cond = false;
+            int contadorDivisores = 0;
+            for (int j = 1;  j < i;  j++) {
+                if (i%j ==0){
+                    contadorDivisores++;
+                }
+            }
+        }
     }
 }
